f946cd1d5e9d40596ed415e17181582273e586cd
BIT <BIT@BIT-PC>
2015-08-05 10:36:20 +0900

    도메인 필터링 수정

--- Project01/src/main/java/org/nojo/filter/DomainInterceptor.java
+++ Project01/src/main/java/org/nojo/filter/DomainInterceptor.java
@@ -12,17 +12,21 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class DomainInterceptor extends HandlerInterceptorAdapter {
 
-  @Inject
-  private DomainMapper domainMapper;
+	@Inject
+	private DomainMapper domainMapper;
 
-  @Override
-  @SuppressWarnings("unchecked")
-  public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object handler) throws Exception {
-    Map<String, String> pathVariables = (Map<String, String>) req.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
-    boolean existence = domainMapper.getAllDomain().contains(pathVariables.get("domain"));
-    if(!existence){
-      res.sendError(HttpServletResponse.SC_NOT_FOUND, "존재하지 않는 도메인 입니다");
-    }
-    return existence;
-  }
+	@Override
+	@SuppressWarnings("unchecked")
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object handler) throws Exception {
+		Map<String, String> pathVariables = (Map<String, String>) req.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		if (pathVariables == null)
+			return true;
+		String domain = pathVariables.get("domain");
+		if (domain == null)
+			return true;
+		boolean existence = domainMapper.getAllDomain().contains(domain);
+		if (!existence)
+			res.sendError(HttpServletResponse.SC_NOT_FOUND, "존재하지 않는 도메인 입니다");
+		return existence;
+	}
 }
