6f7f7b27a0aad2b7a862119af7b2d05a4611c9ef
maingashi <maingashi@gmail.com>
2015-08-11 14:03:52 +0900

    FilemanagerVO 추가 FileattachVO 추가

---
+++ Project01/src/main/java/org/nojo/domain/AttachfileVO.java
@@ -0,0 +1,64 @@
+package org.nojo.domain;
+
+import java.util.Date;
+
+public class AttachfileVO {
+
+	//파일 번호
+	private int attachfile_no;
+	
+	//파일경로	
+	private String attachfile_path;
+	
+	//파일이름
+	private String attachfile_name;
+	
+	//파일크기
+	private int attachfile_size;
+	
+	//등록일
+	private Date attachfile_reg_date;
+	
+	
+	
+	//region Getter Start
+	public int getAttachfile_no() {
+		return attachfile_no;
+	}
+	public String getAttachfile_path() {
+		return attachfile_path;
+	}
+	public String getAttachfile_name() {
+		return attachfile_name;
+	}
+	public int getAttachfile_size() {
+		return attachfile_size;
+	}
+	public Date getAttachfile_reg_date() {
+		return attachfile_reg_date;
+	}
+	//region End
+	
+	
+	
+	
+	//region Setter Start
+	public void setAttachfile_no(int attachfile_no) {
+		this.attachfile_no = attachfile_no;
+	}
+	public void setAttachfile_path(String attachfile_path) {
+		this.attachfile_path = attachfile_path;
+	}
+	public void setAttachfile_name(String attachfile_name) {
+		this.attachfile_name = attachfile_name;
+	}
+	public void setAttachfile_size(int attachfile_size) {
+		this.attachfile_size = attachfile_size;
+	}
+	public void setAttachfile_reg_date(Date attachfile_reg_date) {
+		this.attachfile_reg_date = attachfile_reg_date;
+	}
+	//region End
+	
+
+}
---
+++ Project01/src/main/java/org/nojo/domain/FilemanagerVO.java
@@ -0,0 +1,68 @@
+package org.nojo.domain;
+
+public class FilemanagerVO {
+	
+	
+	//tbl_filemanager VO
+	private int curri_no;
+	
+	//common VO
+	private int attachfile_no;
+	private String clz_domain;
+	
+	
+	//tbl_question_filemanager VO
+	private int question_no;
+	
+	//tbl_answer_filemanager VO
+	private int answer_no;
+
+	
+	//region Getter Start
+	public int getCurri_no() {
+		return curri_no;
+	}
+
+	public int getAttachfile_no() {
+		return attachfile_no;
+	}
+
+	public String getClz_domain() {
+		return clz_domain;
+	}
+
+	public int getQuestion_no() {
+		return question_no;
+	}
+
+	public int getAnswer_no() {
+		return answer_no;
+	}
+	//region End
+
+	
+	
+	//region Setter Start
+	public void setCurri_no(int curri_no) {
+		this.curri_no = curri_no;
+	}
+
+	public void setAttachfile_no(int attachfile_no) {
+		this.attachfile_no = attachfile_no;
+	}
+
+	public void setClz_domain(String clz_domain) {
+		this.clz_domain = clz_domain;
+	}
+
+	public void setQuestion_no(int question_no) {
+		this.question_no = question_no;
+	}
+
+	public void setAnswer_no(int answer_no) {
+		this.answer_no = answer_no;
+	}
+	//region End
+	
+	
+}
