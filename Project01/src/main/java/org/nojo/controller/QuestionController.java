7c113c69f30e1497fd263529ec5e4e7b11166c29
maingashi <maingashi@gmail.com>
2015-08-23 19:17:27 +0900

    08/23 질문게시판 헤더 풋터 추가

--- Project01/src/main/java/org/nojo/controller/QuestionController.java
+++ Project01/src/main/java/org/nojo/controller/QuestionController.java
@@ -84,8 +84,8 @@ public class QuestionController {
 	@Transactional
 	@RequestMapping(value = "/questionRegist", method = RequestMethod.POST)
 	public String registQuestion(QuestionVO vo,
-			@RequestParam(value = "attachfile_no", required = false) Integer[] attachfile_no) throws Exception {
-
+			@RequestParam(value = "attachfile_no", required = false) Integer[] attachfile_no,@PathVariable("domain") String domain) throws Exception {
+		
 		service.addQuestion(vo);
 
 		 if (attachfile_no != null) {
