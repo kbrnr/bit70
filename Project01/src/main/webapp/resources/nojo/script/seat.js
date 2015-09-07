9f6abda3f25b46c23cdedc842f89fcd33472245a
bit <bit@bit-PC>
2015-09-07 16:56:55 +0900

    배치도 수정

--- Project01/src/main/webapp/resources/nojo/script/seat.js
+++ Project01/src/main/webapp/resources/nojo/script/seat.js
@@ -12,12 +12,11 @@ function imgError(image) {
 				var y = this.seat_y;
 				var name = this.mem_name;
 				var id = this.mem_id;
-				var str = "<div class='chair' style='margin-left: "	+ x + "px; margin-top: " + y + "px;'>"
+				var str = "<div class='chair' data-mem_id='" + id + "' data-trigger='manual' data-placement='left' style='margin-left: "	+ x + "px; margin-top: " + y + "px;'>"
 						+ "  <div class='img'>"
 						+			name
 						+ "	    <img class='realImg' src='" + domain + "/seat/seatImg?userId=" + id + "' onerror='imgError(this);'/>"
 						+ "  </div>"
-						+ "  <div class='name' data-mem_id='" + id + "' data-trigger='manual' data-placement='left' data-html='true' ></div>"
 						+ "</div>";
 
 				var chair = $(str);
@@ -30,17 +29,19 @@ function imgError(image) {
 	parent.socket.on("onlineUser", function(users){
 		for(var i in users){
 			console.log("onlineUser: " + users[i]);
-			$(".name[data-mem_id='" + users[i] + "']").parent().children().children($(".realImg")).css( { "border" : "4px solid #34ff57"});
+			$(".chair[data-mem_id='" + users[i] + "']").children($(".img")).css( { "border" : "4px solid #34ff57"});
+			$(".chair[data-mem_id='" + users[i] + "']").children().children($(".img")).css( { "border" : "4px solid #34ff57"});
 		}
 	});
 	parent.socket.on("offlineUser", function(user){
 		console.log("offlineUser: " + user);
-		$(".name[data-mem_id='" + user + "']").parent().children().children($(".realImg")).css( {"border" : "2px solid white", "border-radius" : "30px"});
+		$(".chair[data-mem_id='" + user + "']").children($(".realImg")).css( {"border" : "2px solid white", "border-radius" : "30px"});
 	});
 	
 	//Seat에서 이해도 점수 표시
 	parent.socket.on("seatScore", function(data){
-		var target = $(".name[data-mem_id='"+data.mem_id+"']");
+		console.log(data);
+		var target = $(".chair[data-mem_id='" + data.mem_id + "']");
 		target.attr("data-content", data.comprehension_score);
 		target.popover('show');
 		setTimeout(function(){
@@ -52,7 +53,7 @@ function imgError(image) {
 	parent.socket.on("seatQuestion", function(no){
 		var qno = no.qno;
 		var href = "<a href='" + domain + "/qna/detail?no="+qno+"'><img class='marginImg' src='/resources/nojo/images/questionMark.png' /></a>";
-		var qtarget = $(".name[data-mem_id='"+no.userId+"']").parent().append(href);
+		var qtarget = $(".chair[data-mem_id='"+no.userId+"']").append(href);
  		setTimeout(function(){
 			qtarget.children()[2].remove();
 		}, 60000);
